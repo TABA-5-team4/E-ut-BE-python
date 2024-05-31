4889f1a1c73ac7992112958da70b74773dc57a68
kwon416 <bbbggg111@naver.com>
2024-05-31 14:52:39 +0900

    Feat: latest

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File
 from typing import Union
 from pydantic import BaseModel
 
@@ -38,5 +38,40 @@ def chat(chatRequest: ChatRequest):
     return {"response": response}
 
 @app.post("/stt")
-def stt(request: STTRequest):
-    return {"voiceFile": request.voiceFile}
+def stt(voiceFile: bytes = File(...)):
+    # todo
+    return {
+    "audio_length": 7.983457,
+    "stt_result": "우리 집에 잘 못 산다는 것을 친구들이 알게 되었을 때 정말 억장이 무너지는 거 같았어",
+    "gpt_response": "그렇게 슬픈 생각을 하고 계시네요. 혼자 계시다 보니 더 그런 마음이 들 수 있죠. 언제든지 제가 옆에 있어요. 함께 이야기를 나누면 마음이 편안해질 거에요.",
+    "sentiment_analysis": [
+        {
+            "label": "슬픔",
+            "score": 0.8891498446464539
+        },
+        {
+            "label": "분노",
+            "score": 0.08132488280534744
+        },
+        {
+            "label": "당황",
+            "score": 0.01866663619875908
+        },
+        {
+            "label": "불안",
+            "score": 0.008278260938823223
+        },
+        {
+            "label": "행복",
+            "score": 0.0009516564896330237
+        },
+        {
+            "label": "중립",
+            "score": 0.0009417913970537484
+        },
+        {
+            "label": "혐오",
+            "score": 0.0006868354394100606
+        }
+    ]
+}
